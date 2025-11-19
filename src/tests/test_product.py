c76d97d8898f405e7f9082fbb02d2049f08bf743
Artem <artemiy9999@gmail.com>
2025-11-19 08:31:29 +0300

    Ensure homework package is importable in pytest via conftest

--- src/tests/test_product.py
+++
@@ -1,8 +0,0 @@
-from homework.product import Product
-
-def test_product_init():
-    p = Product("iPhone", "phone", 120000.0, 3)
-    assert p.name == "iPhone"
-    assert p.description == "phone"
-    assert p.price == 120000.0
-    assert p.quantity == 3
