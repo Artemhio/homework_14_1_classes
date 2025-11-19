3a4ec9cdbfd42a922cd81058d21b75c1a317dbce
Artem <artemiy9999@gmail.com>
2025-11-19 08:14:08 +0300

    Configure homework package and tests for Product and Category

--- src/homework/product.py
+++ src/homework/product.py
@@ -1,5 +1,20 @@
 class Product:
-    def __init__(self, name: str, description: str, price: float, quantity: int):
+    """
+    Класс товара.
+
+    :param name: название товара
+    :param description: описание товара
+    :param price: цена товара (с копейками)
+    :param quantity: количество товара в наличии (в штуках)
+    """
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+    ) -> None:
         self.name = name
         self.description = description
         self.price = price
---
+++ src/tests/test_product.py
@@ -0,0 +1,8 @@
+from homework.product import Product
+
+def test_product_init():
+    p = Product("iPhone", "phone", 120000.0, 3)
+    assert p.name == "iPhone"
+    assert p.description == "phone"
+    assert p.price == 120000.0
+    assert p.quantity == 3
